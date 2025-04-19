2a5751ad704ae803148f71f418ec728e17f50676
brian piercy <bjpcjp@gmail.com>
2025-04-19 10:00:30 -0400

    scrapped mermaid test; updated raindrop data links to v20250419

---
+++ update_raindrop_datafile_in_posts.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Directory containing the Jekyll posts
+POST_DIR="${1:-_posts}"
+
+# Expression to find and replace
+OLD_EXPRESSION='{%- assign raindrop = site.data.raindrop.v20250411.rain20250419 %}'
+NEW_EXPRESSION='{%- assign raindrop = site.data.raindrop.v20250419.rain20250419 %}'
+
+# Loop through all files in the directory
+find "$POST_DIR" -type f -name "*.md" -o -name "*.html" | while read -r file; do
+  if grep -Fq "$OLD_EXPRESSION" "$file"; then
+    # Replace the old expression with the new one in-place
+    sed -i "s|$OLD_EXPRESSION|$NEW_EXPRESSION|g" "$file"
+    # Echo the filename
+    echo "$file"
+  fi
+done
