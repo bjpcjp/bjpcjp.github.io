02be8a0324d2f01222f2ea43f48b98e999d5430c
brian piercy <bjpcjp@gmail.com>
2025-04-22 14:32:10 -0400

    updated to raindrop v20250422

--- update_raindrop_datafile_in_posts.sh
+++ update_raindrop_datafile_in_posts.sh
@@ -3,12 +3,12 @@
 # Directory containing the Jekyll posts
 POST_DIR="${1:-_posts}"
 
-# Expression to find and replace
-OLD_EXPRESSION='{%- assign raindrop = site.data.raindrop.v20250411.rain20250419 %}'
-NEW_EXPRESSION='{%- assign raindrop = site.data.raindrop.v20250419.rain20250419 %}'
+# Updated expression to find and replace
+OLD_EXPRESSION='{%- assign raindrop = site.data.raindrop.v20250419.rain20250419 %}'
+NEW_EXPRESSION='{%- assign raindrop = site.data.raindrop.v20250422.export %}'
 
 # Loop through all files in the directory
-find "$POST_DIR" -type f -name "*.md" -o -name "*.html" | while read -r file; do
+find "$POST_DIR" -type f \( -name "*.md" -o -name "*.html" \) | while read -r file; do
   if grep -Fq "$OLD_EXPRESSION" "$file"; then
     # Replace the old expression with the new one in-place
     sed -i "s|$OLD_EXPRESSION|$NEW_EXPRESSION|g" "$file"
