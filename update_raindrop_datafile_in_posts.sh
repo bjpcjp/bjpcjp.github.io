c1bf87b11bf966369b8c7fd3aa099b2973fe84e3
brian piercy <bjpcjp@gmail.com>
2025-05-07 09:52:43 -0400

    removed data/behav/war-33*; added update_raindrop*.sh

--- update_raindrop_datafile_in_posts.sh
+++ update_raindrop_datafile_in_posts.sh
@@ -4,8 +4,11 @@
 POST_DIR="${1:-_posts}"
 
 # Updated expression to find and replace
-OLD_EXPRESSION='{%- assign raindrop = site.data.raindrop.v20250422.export %}'
-NEW_EXPRESSION='{%- assign raindrop = site.data.raindrop.v20250501.export %}'
+# latest: /home/bjpcjp/projects/code/datasets/raindrop/v20250506/fa130d60-7ef1-4ab2-9f19-28ffe76edb37.csv
+
+#OLD_EXPRESSION='{%- assign raindrop = site.data.raindrop.v20250422.export %}'
+OLD_EXPRESSION='{%- assign raindrop = site.data.raindrop.v20250501.export %}'
+NEW_EXPRESSION='{%- assign raindrop = site.data.raindrop.v20250506.v20250506 %}'
 
 # Loop through all files in the directory
 find "$POST_DIR" -type f \( -name "*.md" -o -name "*.html" \) | while read -r file; do
