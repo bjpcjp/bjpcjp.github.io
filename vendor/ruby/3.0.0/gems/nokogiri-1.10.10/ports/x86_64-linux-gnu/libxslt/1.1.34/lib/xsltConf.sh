90a78aa30586c3d106e7f003b4cbdd9784195e21
brian piercy <bjpcjp@gmail.com>
2025-09-30 13:23:57 -0400

    major cleanup - index now a copy of raindrops.html

--- vendor/ruby/3.0.0/gems/nokogiri-1.10.10/ports/x86_64-linux-gnu/libxslt/1.1.34/lib/xsltConf.sh
+++
@@ -1,8 +0,0 @@
-#
-# Configuration file for using the xslt library
-#
-XSLT_LIBDIR="-L/home/bjpcjp/1TB/projects/ruby-jekyll/github-20220527/bjpcjp.github.io/vendor/ruby/3.0.0/gems/nokogiri-1.10.10/ports/x86_64-linux-gnu/libxslt/1.1.34/lib"
-XSLT_LIBS="-lxslt -L/home/bjpcjp/1TB/projects/ruby-jekyll/github-20220527/bjpcjp.github.io/vendor/ruby/3.0.0/gems/nokogiri-1.10.10/ports/x86_64-linux-gnu/libxml2/2.9.10/lib -lxml2 -lz -lm "
-XSLT_PRIVATE_LIBS="-lm"
-XSLT_INCLUDEDIR="-I/home/bjpcjp/1TB/projects/ruby-jekyll/github-20220527/bjpcjp.github.io/vendor/ruby/3.0.0/gems/nokogiri-1.10.10/ports/x86_64-linux-gnu/libxslt/1.1.34/include"
-MODULE_VERSION="xslt-1.1.34"
